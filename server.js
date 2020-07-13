12c0fd2effce84af72c86577379ddbbffd44114e
Nicole Remy <nicoleremy95@gmail.com>
2020-07-12 18:56:10 -0700

    notes are being added to api and are appearing in list

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static( 'public'));
+
+//variables
+const noteListItems = []
 
 // Routes
 // =============================================================
+//GET
 app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "index.html"));
+res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "notes.html"));
+res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-
-
-//Create a New Reservation 
+app.get("/api/notes", function(req, res) {
+    return res.json(noteListItems)
+    });
+//POST
 app.post("/api/notes", function(req, res) {
-
+    let activeNote = req.body
+    noteListItems.push(activeNote)
+    res.json(noteListItems)
+    console.log(noteListItems)
 });
 
-app.delete("/api/notes/:id", function(req, res) {
+//DELETE
+// app.delete("/api/notes/:id", function(req, res) {
 
-}); 
+// }); 
   
   
-  // Starts the server to begin listening
-  // =============================================================
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
 console.log("App listening on PORT " + PORT);
 });
